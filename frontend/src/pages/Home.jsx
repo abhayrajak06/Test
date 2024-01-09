164a65267b02a09eb536395f50df4fc7e8661c4e
Abhay Rajak <mail2rajab01@gmail.com>
2024-01-09 23:43:24 +0530

    Homeposts fetched from database

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HomePosts from "../components/HomePosts";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import axios from "axios";
+import { URL } from "../url";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const getAllPosts = async () => {
+    try {
+      const res = await axios.get(`${URL}/api/v1/post/`);
+      if (res?.data) {
+        setPosts(res.data);
+        // console.log(res);
+        // console.log(posts);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllPosts();
+  }, []);
   return (
     <>
       <Navbar />
 
       <div className="px-8">
-        <HomePosts />
-        <HomePosts />
-        <HomePosts />
-        <HomePosts />
+        {posts?.map((p) => (
+          <HomePosts key={p._id} post={p} />
+        ))}
       </div>
       <Footer />
     </>
