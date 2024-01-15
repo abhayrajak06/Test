913858e39a7c06611c7a8d786361bffe6153c28d
Abhay Rajak <mail2rajab01@gmail.com>
2024-01-15 23:17:58 +0530

    image upload functionlaity implemented at backend

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/commentRoutes.js";
+import multer from "multer";
 
 const app = express();
 
@@ -24,6 +25,34 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/post", postRoutes);
 app.use("/api/v1/comment", commentRoutes);
+
+//image upload
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images/"); // Specify the destination folder
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname); // Specify the filename
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/v1/upload", upload.single("file"), (req, res) => {
+  try {
+    if (!req.file) {
+      throw new Error("No file uploaded");
+    }
+
+    // Additional logic for successful file upload
+
+    res.status(200).json("Image has been uploaded successfully");
+  } catch (error) {
+    console.error(error);
+    res
+      .status(400)
+      .json({ message: error.message || "Failed to upload image" });
+  }
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running...");
