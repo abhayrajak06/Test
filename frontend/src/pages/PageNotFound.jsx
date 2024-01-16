e0e62f7bbe86301d6de74c54a98312b02d1efe1c
Abhay Rajak <mail2rajab01@gmail.com>
2024-01-16 23:58:42 +0530

    pagenotfound page's UI improved

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const PageNotFound = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <div className="bg-slate-600" style={{ minHeight: "74vh" }}>
+    <div className="bg-slate-300" style={{ minHeight: "100vh" }}>
       <div
         className="flex flex-col items-center justify-center"
-        style={{ height: "60vh" }}
+        style={{ height: "100vh" }}
       >
-        <h1 className="text-9xl font-bold ">404</h1>
-        <h2 className="text-2xl m-2">Page Not Found !</h2>
+        <h1 className="text-9xl font-bold">404</h1>
+        <h2 className="text-2xl m-2">Page Not Found!</h2>
+        <button onClick={goBack} className="btn bg-slate-600 p-2 mt-1 rounded">
+          Go Back
+        </button>
       </div>
     </div>
   );
