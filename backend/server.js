0793537e3532bd0a6a08849a7920434e70a3b92d
Abhay Rajak <mail2rajab01@gmail.com>
2024-02-16 18:11:07 +0530

    test2

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectDB();
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://test-y43y.vercel.app/", credentials: true }));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/post", postRoutes);
